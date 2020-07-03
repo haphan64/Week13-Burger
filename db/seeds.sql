84a1d0079afb4f43b1a8b76ed38056e7eebdd439
Ha Phan <haphan64@outlook.com>
2020-07-03 01:24:37 -0700

    add burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Shackburger');
+INSERT INTO burgers (burger_name) VALUES ('Truffle Mushroom Swiss Smashed Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Animal Style', true);
+
